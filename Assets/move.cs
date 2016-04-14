496e7f7a13f43122fbd415c0496343ec0208a91b
lorenzo456 <lorenzo456_@hotmail.com>
2016-04-14 23:11:56 +0200

    initial

---
+++ Assets/move.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+    public float min;
+    public float max;
+    public string input;
+
+    float currentRotation;
+    float currentMovement;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentRotation = gameObject.transform.rotation.y;
+        currentMovement = Input.GetAxis(input);
+       // if(currentMovement > 0 && currentMovement < 1)
+       if(currentRotation >= min && currentRotation < max)
+            currentRotation = currentMovement;
+
+        gameObject.transform.rotation = new Quaternion(transform.rotation.x,Mathf.Abs(currentRotation), transform.rotation.z, transform.rotation.w);
+        Debug.Log(currentRotation);
+	}
+}
